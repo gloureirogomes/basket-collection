97c0f5b1e4ce8e79de843f1545397e6a865fd765
Gabriel <loureirobiel@gmail.com>
2023-05-01 14:25:50 -0300

    add gin gonic http framework

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Alo porra")
+	r := gin.Default()
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Funcionando!",
+		})
+	})
+	r.Run()
 }
