3c16a887fe2f045091821fd665e46331e19464d3
Gabriel <loureirobiel@gmail.com>
2023-05-04 01:19:00 -0300

    create player contract

--- internal/core/service/player.go
+++ internal/core/service/player.go
@@ -2,11 +2,13 @@ package service
 
 import (
 	"context"
+
+	"github.com/GabrielLoureiroGomes/basket-collection/internal/core/domain"
 )
 
 // Player interface is used to manage player functions
 type Player interface {
 
 	// InsertPlayer function is used to create a player register
-	InsertPlayer(ctx context.Context) error
+	InsertPlayer(ctx context.Context, player domain.Player) error
 }
