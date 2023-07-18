d18e2059c4599257c8a0a5a21c85f0d81abf34b5
Gabriel Loureiro Gomes <loureirobiel@gmail.com>
2023-07-17 22:00:08 -0300

    create function to test authentication

--- pkg/repository/mongo.go
+++ pkg/repository/mongo.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"fmt"
+	"net"
 
 	"github.com/GabrielLoureiroGomes/basket-collection/logger"
 	"github.com/spf13/viper"
@@ -22,10 +23,15 @@ type mongoClient struct {
 
 // newMongoClient is used to connect on mongoDB
 func newMongoClient(ctx context.Context) *mongoClient {
+	host := viper.GetString("MONGO_HOST")
+	port := viper.GetString("MONGO_PORT")
 	user := viper.GetString("MONGO_USER")
 	password := viper.GetString("MONGO_PASSWORD")
 	databaseName := viper.GetString("MONGO_DATABASE_NAME")
 	stringConnection := fmt.Sprintf("mongodb+srv://%s:%s@%s.rzlfysv.mongodb.net/?retryWrites=true&w=majority", user, password, databaseName)
+	if noAuthentication(user, password) {
+		stringConnection = fmt.Sprintf("mongodb://%s", net.JoinHostPort(host, port))
+	}
 
 	opts := options.Client().ApplyURI(stringConnection)
 	client, err := mongo.Connect(ctx, opts)
@@ -37,4 +43,8 @@ func newMongoClient(ctx context.Context) *mongoClient {
 	return &mongoClient{
 		client: client,
 	}
+}
+
+func noAuthentication(user, password string) bool {
+	return user == "" && password == ""
 }
