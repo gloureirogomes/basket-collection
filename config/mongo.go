d9ba3dc99c1f5daea96a8fadf6472ac1be260f3c
Gabriel Loureiro Gomes <loureirobiel@gmail.com>
2023-07-17 22:38:07 -0300

    add environment variable for mongo host and port

--- config/mongo.go
+++ config/mongo.go
@@ -3,6 +3,8 @@ package config
 import "github.com/spf13/viper"
 
 func mongoConfig() {
+	viper.SetDefault("MONGO_HOST", "localhost")
+	viper.SetDefault("MONGO_PORT", "27017")
 	viper.SetDefault("MONGO_USER", "basket")
 	viper.SetDefault("MONGO_PASSWORD", "password-here")
 	viper.SetDefault("MONGO_DATABASE_NAME", "basket-collection")
